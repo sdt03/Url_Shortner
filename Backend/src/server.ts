d6149cba39655bc7578b625a5e9bf6e29f8ddbd8
sdt03 <dateraoshoumik@gmail.com>
2025-01-14 17:23:20 +0530

    mongodb connections made

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,11 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';   
+import connectDB from './config/dbConfig';
 dotenv.config();   
+connectDB();
 
 const port = process.env.PORT || 5001;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
